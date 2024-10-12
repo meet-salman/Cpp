c89cd00fb433a9164bab5e1cbe17b63fa29cfb0d
Salman Ahmed <say2salmanahmed@gmail.com>
2024-10-12 18:12:13 +0500

    Find nth Number in Fibonacci Series

--- Generate Fibonacci Series/main.cpp
+++ Generate Fibonacci Series/main.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
 using namespace std;
 
-// --------------------------------------------------------------
-// GENERATE FIBONANCCI SERIES
-// --------------------------------------------------------------
+// // --------------------------------------------------------------
+// // GENERATE FIBONANCCI SERIES
+// // --------------------------------------------------------------
 
-int generateFibonacciSeries(int range)
+int generate_Fibonacci_Series(int range)
 {
     int a = 0,
         b = 1,
@@ -23,19 +23,52 @@ int generateFibonacciSeries(int range)
     return 0;
 }
 
+// // --------------------------------------------------------------
+// // FIND Nth NUMBER IN FIBONANCCI SERIES
+// // --------------------------------------------------------------
+
+// int find_Num_In_Fibonacci_Series(int num)
+// {
+//     int a = 0,
+//         b = 1,
+//         c = 0;
+
+//     for (int i = 1; i <= num; i++)
+//     {
+//         a = b;
+//         b = c;
+//         c = a + b;
+//     }
+
+//     cout << "\nThe number at " << num << "th in Finonancci series: " << c << endl;
+
+//     return 0;
+// }
+
 int main()
 {
 
-    // --------------------------------------------------------------
-    // GENERATE FIBONANCCI SERIES
-    // --------------------------------------------------------------
+    // // --------------------------------------------------------------
+    // // GENERATE FIBONANCCI SERIES
+    // // --------------------------------------------------------------
 
     int range;
 
     cout << "Enter Range" << endl;
     cin >> range;
 
-    generateFibonacciSeries(range);
+    generate_Fibonacci_Series(range);
+
+    // // --------------------------------------------------------------
+    // // FIND Nth NUMBER IN FIBONANCCI SERIES
+    // // --------------------------------------------------------------
+
+    // int num;
+
+    // cout << "Enter Number" << endl;
+    // cin >> num;
+
+    // find_Num_In_Fibonacci_Series(num);
 
     return 0;
 }

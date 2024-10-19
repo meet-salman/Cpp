3ddcad2aa191099091aae9c44d13ac481b25dc3e
Salman Ahmed <say2salmanahmed@gmail.com>
2024-10-19 13:05:53 +0500

    Target Sum of Two Numbers in Array

--- Target Sum of Two Numbers in Array/main.cpp
+++ Target Sum of Two Numbers in Array/main.cpp
@@ -4,19 +4,20 @@ using namespace std;
 int main()
 {
 
-    int nums[] = {2, 5, 6, 7, 9};
+    int nums[] = {4, 13, 7, 9, 2};
     int target = 11;
 
-    for (int i = 0; i < sizeof(nums); i++)
+    for (int i = 0; i < end(nums) - begin(nums); i++)
     {
-        int j = i + 1;
 
-        if (nums[i] + nums[j] == target)
+        for (int j = i + 1; j < end(nums) - begin(nums); j++)
         {
-            cout << nums[i] << endl;
-            cout << nums[j] << endl;
+            if (nums[i] + nums[j] == target)
+            {
+                cout << nums[i] << ", " << nums[j] << endl;
 
-            break;
+                break;
+            }
         }
     }
 

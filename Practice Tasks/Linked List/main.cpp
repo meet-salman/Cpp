1ea388cb0216085b416544b177bf04dace9fd94e
Salman Ahmed <say2salmanahmed@gmail.com>
2024-12-20 21:26:54 +0500

    Update Add, Update Function

--- Practice Tasks/Linked List/main.cpp
+++ Practice Tasks/Linked List/main.cpp
@@ -4,31 +4,29 @@ using namespace std;
 struct Node
 {
     string value;
-    int idx;
+    int index;
     Node *next;
+
+    Node(string val, int idx) : value(val), index(idx), next(nullptr) {}
 };
 
 struct linkedList
 {
-    Node *first;
-    Node *last;
+    Node *first = nullptr;
+    Node *last = nullptr;
     int length = 0;
 
     void addValue(string val)
     {
-        // Creating new Node and setting all values
-        Node *n = new Node;
-        n->value = val;
-        n->idx = length;
-        n->next = nullptr;
+        // Create new node with the given value and index
+        Node *n = new Node(val, length);
 
-        // If length == 0 -> first & last both = n
-        if (length == 0)
+        // If list empty -> first & last both = n
+        if (!first)
         {
-            first = n;
-            last = n;
+            first = last = n;
         }
-        // If length > 0 -> first always same - last jump to n - first pointing n
+        // If list has some values -> first always same - last jump to n - first pointing n
         else
         {
             last->next = n;
@@ -40,12 +38,19 @@ struct linkedList
     // Display Function -> all values
     void display()
     {
-        // Creating temp Node for treversing
+        // Check if the list is empty
+        if (!first)
+        {
+            cout << "List is empty!" << endl;
+            return;
+        }
+
+        // Creating temp Node for traverse
         Node *temp = first;
 
-        for (int i = 0; i < length; i++)
+        while (temp)
         {
-            cout << "Index: " << temp->idx << "  " << "Value: " << temp->value << endl;
+            cout << "Index: " << temp->index << "  " << "Value: " << temp->value << endl;
             temp = temp->next;
         }
     }
@@ -64,9 +69,9 @@ struct linkedList
 
         for (int i = 0; i <= idx; i++)
         {
-            if (idx == temp->idx)
+            if (idx == temp->index)
             {
-                cout << "Index: " << temp->idx << "  " << "Value: " << temp->value << endl;
+                cout << "Index: " << temp->index << "  " << "Value: " << temp->value << endl;
             }
             temp = temp->next;
         }
@@ -81,7 +86,7 @@ struct linkedList
         {
             if (value == temp->value)
             {
-                cout << "Value: " << temp->value << "  " << "Index: " << temp->idx << endl;
+                cout << "Value: " << temp->value << "  " << "Index: " << temp->index << endl;
                 return;
             }
             temp = temp->next;
@@ -103,7 +108,7 @@ struct linkedList
 
         for (int i = 0; i <= idx; i++)
         {
-            if (idx == temp->idx)
+            if (idx == temp->index)
             {
                 temp->value = value;
                 cout << "Value Updated" << endl;
@@ -130,6 +135,41 @@ struct linkedList
         }
         cout << "Value not found" << endl;
     }
+
+    void deleteValueAtIndex(int idx)
+    {
+        if (idx == 0)
+        {
+            first = first->next;
+            length--;
+        }
+        else
+        {
+            Node *temp = first;
+            Node *prev = temp;
+            for (int i = 0; i <= idx; i++)
+            {
+                if (idx == temp->index)
+                {
+                    prev->next = temp->next;
+                    length--;
+                    break;
+                }
+                prev = temp;
+                temp = temp->next;
+            }
+        }
+
+        Node *temp = first;
+        for (int i = 0; i < length; i++)
+        {
+            if (temp->index > idx)
+            {
+                temp->index--;
+            }
+            temp = temp->next;
+        }
+    }
 };
 
 int main()
@@ -140,25 +180,33 @@ int main()
     list.addValue("Salman Ahmed");
     list.addValue("Suheer Khan");
     list.addValue("Subhan Bashir");
+    list.addValue("Abeer Khan");
+    list.addValue("Talha Sohaib");
+    list.addValue("Zohain Gul");
 
     list.display();
     cout << endl;
 
-    list.findValueAtIndex(2);
-    cout << endl;
+    // list.findValueAtIndex(2);
+    // cout << endl;
 
-    list.findValue("Salman Ahmed");
-    cout << endl;
+    // list.findValue("Salman Ahmed");
+    // cout << endl;
 
-    list.updateValueAtIndex(1, "Shaheer Ahmed");
+    // list.updateValueAtIndex(1, "Shaheer Ahmed");
 
-    list.display();
-    cout << endl;
+    // list.display();
+    // cout << endl;
 
-    list.updateValue("Shaheer Ahmed", "Abeer Khan");
+    // list.updateValue("Shaheer Ahmed", "Abeer Khan");
 
-    list.display();
-    cout << endl;
+    // list.display();
+    // cout << endl;
+
+    // list.deleteValueAtIndex(2);
+
+    // list.display();
+    // cout << endl;
 
     return 0;
 }

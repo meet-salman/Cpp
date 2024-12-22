5030bab8d08cb64c78bd1007cfdc13d9e52267f8
Salman Ahmed <say2salmanahmed@gmail.com>
2024-12-22 19:10:58 +0500

    Decreasing Number Triangle

--- Practice Tasks/Problems/Patterns/Decreasing Number Triangle/main.cpp
+++ Practice Tasks/Problems/Patterns/Decreasing Number Triangle/main.cpp
@@ -3,13 +3,24 @@ using namespace std;
 
 int main()
 {
-    for (int i = 1; i <= 4; i++)
+    // for (int i = 1; i <= 4; i++)
+    // {
+    //     int count = 0;
+    //     for (int j = 4; j >= i; j--)
+    //     {
+    //         count++;
+    //         cout << count << " ";
+    //     }
+    //     cout << endl;
+    // }
+
+    // // --------X--------X--------X--------
+
+    for (int i = 4; i >= 1; i--)
     {
-        int count = 0;
-        for (int j = 4; j >= i; j--)
+        for (int j = 1; j <= i; j++)
         {
-            count++;
-            cout << count << " ";
+            cout << j << " ";
         }
         cout << endl;
     }

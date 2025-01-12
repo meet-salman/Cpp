f8b697fe411fc09757a49acb0208f1718d5fdcb5
Salman Ahmed <say2salmanahmed@gmail.com>
2025-01-12 19:15:24 +0500

    GCD of Two Numbers Using Recursion

--- Practice Tasks/Recursion/GCD of Two Numbers/main.cpp
+++ Practice Tasks/Recursion/GCD of Two Numbers/main.cpp
@@ -3,14 +3,18 @@ using namespace std;
 
 int gcd(int a, int b)
 {
-    if (a == 0 || b == 0)
-        return 0;
-    else if (a == b)
+    // if (a == 0 || b == 0)
+    //     return 0;
+    // else if (a == b)
+    //     return a;
+    // else if (a > b)
+    //     return gcd(a - b, b);
+    // else
+    //     return gcd(a, b - a);
+
+    if (b == 0)
         return a;
-    else if (a > b)
-        return gcd(a - b, b);
-    else
-        return gcd(a, b - a);
+    return gcd(b, a % b);
 }
 
 int main()

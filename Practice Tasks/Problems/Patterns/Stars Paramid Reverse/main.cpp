f140baf739203e4cf041a646728b1f026b571d96
Salman Ahmed <say2salmanahmed@gmail.com>
2025-09-04 16:50:05 +0500

    Stars Paramid Reversed

--- Practice Tasks/Problems/Patterns/Stars Paramid Reverse/main.cpp
+++ Practice Tasks/Problems/Patterns/Stars Paramid Reverse/main.cpp
@@ -2,24 +2,50 @@
 #include <string>
 using namespace std;
 
+// void print_pattern(int n)
+// {
+//     for (int i = n; i > 0; i--)
+//     {
+//         // spaces
+//         for (int j = 0; j < n - i; j++)
+//         {
+//             cout << " ";
+//         }
+
+//         // Stars
+//         for (int k = 0; k < (2 * i) - 1; k++)
+//         {
+//             cout << "*";
+//         }
+
+//         // spaces
+//         for (int j = 0; j < n - i; j++)
+//         {
+//             cout << " ";
+//         }
+
+//         cout << endl;
+//     }
+// }
+
 void print_pattern(int n)
 {
-    for (int i = n; i > 0; i--)
+    for (int i = 0; i < n; i++)
     {
         // spaces
-        for (int j = 0; j < n - i; j++)
+        for (int j = 0; j < i; j++)
         {
             cout << " ";
         }
 
         // Stars
-        for (int k = 0; k < (2 * i) - 1; k++)
+        for (int k = 0; k < ((2 * n) - (2 * i + 1)); k++)
         {
             cout << "*";
         }
 
         // spaces
-        for (int j = 0; j < n - i; j++)
+        for (int j = 0; j < i; j++)
         {
             cout << " ";
         }
